9fe033228a1a204a41fe65e677424d78fad442d2
Katya1803 <vuvuworkplace@gmail.com>
2024-06-09 21:04:10 +0700

    add concurrency

---
+++ src/concurrency/Order.java
@@ -0,0 +1,46 @@
+package concurrency;
+
+public class Order {
+    private int orderId;
+    private int productId;
+    private int quantity;
+
+    public Order(int orderId, int productId, int quantity) {
+        this.orderId = orderId;
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
